f5827addad81cfd2a8dab27330dad2334af878e6
Cristian Radulescu <cristian67@gmail.com>
2016-11-13 20:25:07 +0200

    Install Sonata Admin
    New database model. Code cleanup.

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -16,6 +16,5 @@ class AppExtension extends Extension
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
-        $loader->load('menu.yml');
     }
 }
