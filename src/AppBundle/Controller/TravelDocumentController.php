4bd07b7ca1b5109f5868cda8429ae407439dbb13
Cristian Radulescu <cristian67@gmail.com>
2016-12-07 17:09:13 +0200

    Move some logic from controller to service.

--- src/AppBundle/Controller/TravelDocumentController.php
+++ src/AppBundle/Controller/TravelDocumentController.php
@@ -4,11 +4,16 @@ namespace AppBundle\Controller;
 
 
 use AppBundle\Entity\TravelDocument;
+use AppBundle\Service\TravelDocumentService;
 use Sonata\AdminBundle\Controller\CRUDController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
+/**
+ * Class TravelDocumentController
+ * @package AppBundle\Controller
+ */
 class TravelDocumentController extends CRUDController
 {
     /**
@@ -22,36 +27,19 @@ class TravelDocumentController extends CRUDController
     {
         /** @var TravelDocument $travelDocument */
         $travelDocument = $this->admin->getSubject();
+
+        /** @var TravelDocumentService $travelDocumentService */
+        $travelDocumentService = $this->get('app.travel_document');
+        $placeholders = $travelDocumentService->fillPlaceholders($travelDocument);
+
         $pages['ordin_de_deplasare_p1.svg'] = $this->render(
             'AppBundle:svg:ordin_de_deplasare_p1.svg.twig',
-            array(
-                'PLACEHOLDER_COST_CENTER' => $travelDocument->getEmployee()->getCompany()->getCostCenter(),
-                'PLACEHOLDER_EMPLOYEE_NAME' => $travelDocument->getEmployee()->getFullName(),
-                'PLACEHOLDER_EMPLOYEE_JOB_TITLE' => $travelDocument->getEmployee()->getJobTitle(),
-                'PLACEHOLDER_TRAVEL_PURPOSE' => $travelDocument->getPurpose(),
-                'PLACEHOLDER_TRAVEL_DESTINATION' => $travelDocument->getDestination(),
-                'PLACEHOLDER_COMPANY_NAME' => $travelDocument->getEmployee()->getCompany()->getName(),
-                'PLACEHOLDER_EMPLOYEE_DETAILS' => $travelDocument->getEmployee()->getPersonalNumericCode(),
-                'PLACEHOLDER_DATE_FROM' => $travelDocument->getDateStart()->format('d.m.Y'),
-                'PLACEHOLDER_DATE_TO' => $travelDocument->getDateEnd()->format('d.m.Y'),
-                'PLACEHOLDER_DESTINATION_ARRIVAL_TIME' => $travelDocument->getDestinationArrivalTime()->format('d.m.Y H:i'),
-                'PLACEHOLDER_DESTINATION_LEAVE_TIME' => $travelDocument->getDestinationLeaveTime()->format('d.m.Y H:i'),
-            )
+            $placeholders['ordin_de_deplasare_p1.svg']
         );
 
         $pages['ordin_de_deplasare_p2.svg'] = $this->render(
             'AppBundle:svg:ordin_de_deplasare_p2.svg.twig',
-            array(
-                'PLACEHOLDER_STARTPOINT_LEAVE_TIME' => $travelDocument->getDepartureLeaveTime()->format('d.m.Y H:i'),
-                'PLACEHOLDER_STARTPOINT_ARRIVAL_TIME' => $travelDocument->getDepartureArrivalTime()->format('d.m.Y H:i'),
-                'PLACEHOLDER_DOCUMENT_TYPE' => 'Diurna 1 zi',
-                'PLACEHOLDER_AMOUNT' => '32.5',
-                'PLACEHOLDER_TOTAL_AMOUNT' => '32.5',
-                'PLACEHOLDER_MANAGER_LAST_NAME' => $travelDocument->getEmployee()->getCompany()->getDivisionManager()->getLastName(),
-                'PLACEHOLDER_MANAGER_FIRST_NAME' => $travelDocument->getEmployee()->getCompany()->getDivisionManager()->getFirstName(),
-                'PLACEHOLDER_EMPLOYEE_LAST_NAME' => $travelDocument->getEmployee()->getLastName(),
-                'PLACEHOLDER_EMPLOYEE_FIRST_NAME' => $travelDocument->getEmployee()->getFirstName()
-            )
+            $placeholders['ordin_de_deplasare_p2.svg']
         );
 
         // create zip file to download both pages at once
@@ -86,6 +74,10 @@ class TravelDocumentController extends CRUDController
         $clonedObject = clone $object;
         $clonedObject = $this->admin->create($clonedObject);
 
+        $this->addFlash(
+            'sonata_flash_success',
+            'The travel document was successfully cloned. Please edit the details of the new document.'
+        );
         return new RedirectResponse($this->admin->generateUrl('edit', array('id' => $clonedObject->getId())));
     }
 }
---
+++ src/AppBundle/Service/TravelDocumentService.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace AppBundle\Service;
+
+use AppBundle\Entity\TravelDocument;
+
+/**
+ * Class TravelDocumentService
+ * @package AppBundle\Service
+ */
+class TravelDocumentService
+{
+
+    const DATE_FORMAT_PATTERN = 'd.m.Y H:i';
+    const TRAVEL_DAY_PAYMENT = 32.5;
+    const DOCUMENT_TYPE_TEXT = 'Diurna %s zile (%s lei/zi)';
+
+    /**
+     * @param TravelDocument $travelDocument
+     * @return array
+     */
+    public function fillPlaceholders(TravelDocument $travelDocument)
+    {
+        $employee = $travelDocument->getEmployee();
+        $company = $employee->getCompany();
+
+        $placeholders['ordin_de_deplasare_p1.svg'] = array(
+            'PLACEHOLDER_COST_CENTER' => $company->getCostCenter(),
+            'PLACEHOLDER_EMPLOYEE_NAME' => $employee->getFullName(),
+            'PLACEHOLDER_EMPLOYEE_JOB_TITLE' => $employee->getJobTitle(),
+            'PLACEHOLDER_TRAVEL_PURPOSE' => $travelDocument->getPurpose(),
+            'PLACEHOLDER_TRAVEL_DESTINATION' => $travelDocument->getDestination(),
+            'PLACEHOLDER_COMPANY_NAME' => $company->getName(),
+            'PLACEHOLDER_EMPLOYEE_DETAILS' => $employee->getPersonalNumericCode(),
+            'PLACEHOLDER_DATE_FROM' => $travelDocument->getDateStart()->format('d.m.Y'),
+            'PLACEHOLDER_DATE_TO' => $travelDocument->getDateEnd()->format('d.m.Y'),
+            'PLACEHOLDER_DESTINATION_ARRIVAL_TIME' => $travelDocument->getDestinationArrivalTime()
+                ->format(self::DATE_FORMAT_PATTERN),
+            'PLACEHOLDER_DESTINATION_LEAVE_TIME' => $travelDocument->getDestinationLeaveTime()
+                ->format(self::DATE_FORMAT_PATTERN),
+        );
+
+        $daysOnTravel = $this->computeDaysOnTravelByTravelDates(
+            $travelDocument->getDateStart(),
+            $travelDocument->getDateEnd()
+        );
+        $paymentAmount = $daysOnTravel * self::TRAVEL_DAY_PAYMENT;
+
+        $placeholders['ordin_de_deplasare_p2.svg'] = array(
+            'PLACEHOLDER_STARTPOINT_LEAVE_TIME' => $travelDocument->getDepartureLeaveTime()
+                ->format(self::DATE_FORMAT_PATTERN),
+            'PLACEHOLDER_STARTPOINT_ARRIVAL_TIME' => $travelDocument->getDepartureArrivalTime()
+                ->format(self::DATE_FORMAT_PATTERN),
+            'PLACEHOLDER_DOCUMENT_TYPE' => sprintf(self::DOCUMENT_TYPE_TEXT, $daysOnTravel, self::TRAVEL_DAY_PAYMENT),
+            'PLACEHOLDER_AMOUNT' => $paymentAmount,
+            'PLACEHOLDER_TOTAL_AMOUNT' => $paymentAmount,
+            'PLACEHOLDER_MANAGER_LAST_NAME' => $company->getDivisionManager()->getLastName(),
+            'PLACEHOLDER_MANAGER_FIRST_NAME' => $company->getDivisionManager()->getFirstName(),
+            'PLACEHOLDER_EMPLOYEE_LAST_NAME' => $employee->getLastName(),
+            'PLACEHOLDER_EMPLOYEE_FIRST_NAME' => $employee->getFirstName()
+        );
+
+        return $placeholders;
+    }
+
+    /**
+     * Determine the document type (payment for days spent travelling).
+     *
+     * Must add 1 day to the final result in order to include the first day, since the date difference method will not
+     * include it.
+     *
+     * @param \DateTime $startDate
+     * @param \DateTime $endDate
+     * @return string
+     */
+    protected function computeDaysOnTravelByTravelDates(\DateTime $startDate, \DateTime $endDate)
+    {
+        $travelTime = $endDate->diff($startDate);
+
+        return $travelTime->days + 1;
+    }
+}
