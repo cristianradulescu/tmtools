8b29ef2068fd1c4aa6b5e4588d272cb339da7fd8
Cristian Radulescu <cristian67@gmail.com>
2016-12-21 19:22:43 +0200

    Basic authentication

--- data/sql/tmtools.sql
+++ data/sql/tmtools.sql
@@ -319,6 +319,33 @@ CREATE INDEX `fk_reinbursement_idx` ON `tmtools`.`reinbursement_document_reinbur
 
 
 -- -----------------------------------------------------
+-- Table `tmtools`.`fos_user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tmtools`.`fos_user` ;
+
+CREATE TABLE `tmtools`.`fos_user` (
+  id INT AUTO_INCREMENT NOT NULL,
+  username VARCHAR(180) NOT NULL,
+  username_canonical VARCHAR(180) NOT NULL,
+  email VARCHAR(180) NOT NULL,
+  email_canonical VARCHAR(180) NOT NULL,
+  enabled TINYINT(1) NOT NULL,
+  salt VARCHAR(255) DEFAULT NULL,
+  PASSWORD VARCHAR(255) NOT NULL,
+  last_login DATETIME DEFAULT NULL,
+  confirmation_token VARCHAR(180) DEFAULT NULL,
+  password_requested_at DATETIME DEFAULT NULL,
+  roles LONGTEXT NOT NULL COMMENT '(DC2Type:array)',
+  UNIQUE INDEX UNIQ_957A647992FC23A8 (username_canonical),
+  UNIQUE INDEX UNIQ_957A6479A0D96FBF (email_canonical),
+  UNIQUE INDEX UNIQ_957A6479C05FB297 (confirmation_token),
+  PRIMARY KEY(id))
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_unicode_ci
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Data for table `tmtools`.`employee_job_title`
 -- -----------------------------------------------------
 START TRANSACTION;
