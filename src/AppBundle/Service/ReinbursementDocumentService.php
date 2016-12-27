0db899ece3419ae224d0fe3cd59883aec1f0bb00
Cristian Radulescu <cristian67@gmail.com>
2016-12-27 11:03:42 +0200

    Code cleanup, date format fix.

--- src/AppBundle/Service/ReinbursementDocumentService.php
+++ src/AppBundle/Service/ReinbursementDocumentService.php
@@ -19,15 +19,6 @@ class ReinbursementDocumentService extends DocumentService
     public function fillPlaceholders(EmployeeInterface $reinbursementDocument)
     {
         parent::fillPlaceholders($reinbursementDocument);
-        $reinbursementPlaceholders = [];
-        $reinbursementTotalAmount = 0;
-        $reinbursementCollection = $reinbursementDocument->getReinbursement();
-
-        foreach ($reinbursementCollection as $key => $reinbursement) {
-            /** @var Reinbursement $reinbursement */
-            $reinbursementTotalAmount +=
-                $reinbursementPlaceholders['PLACEHOLDER_DOC_AMOUNT_'.$key] = floatval($reinbursement->getValue());
-        }
 
         return array(
             'PLACEHOLDER_COMPANY_NAME' => $this->getCompany()->getName(),
@@ -36,8 +27,7 @@ class ReinbursementDocumentService extends DocumentService
             'PLACEHOLDER_EMPLOYEE_JOB_TITLE' => $this->getEmployee()->getJobTitle(),
             'PLACEHOLDER_DIVISION_MANAGER_LAST_NAME' => $this->getCompany()->getDivisionManager()->getLastName(),
             'PLACEHOLDER_DIVISION_MANAGER_FIRST_NAME' => $this->getCompany()->getDivisionManager()->getFirstName(),
-            'PLACEHOLDER_TOTAL_AMOUNT' => $reinbursementTotalAmount,
-            'reinbursement_collection' => $reinbursementCollection
+            'reinbursement_collection' => $reinbursementDocument->getReinbursement()
         );
     }
 }
